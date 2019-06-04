3721019d1023b96acfa19908675a480d4388f69d
VeraRi <44agata.samborska@gmail.com>
2019-06-04 17:47:14 +0200

    koniec

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public int lives;
+    public int score;
+    public Text livesText;
+    public Text scoreText;
+    public bool gameOver;
+    public bool nextLevel;
+    public GameObject gameOverPanel;
+    public GameObject nextLevelPanel;
+    public int numberOfBricks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        livesText.text = "Lives:" + lives;
+        scoreText.text = "Score:" + score;
+        numberOfBricks = GameObject.FindGameObjectsWithTag("brick").Length;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateLives (int changeInLives)
+    {
+        lives += changeInLives;
+        if(lives <= 0)
+        {
+            lives = 0;
+            GameOver();
+        }
+        livesText.text = "Lives:" + lives;
+
+    }
+    public void UpdateScore(int points)
+    {
+        score += points;
+        scoreText.text = "Score:" + score;
+
+    }
+
+    public void UpdateNumberOfBricks()
+    {
+        numberOfBricks--;
+        if(numberOfBricks <= 0)
+        {
+            NextLevel ();
+        }
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void GameOver()
+    {
+        gameOver = true;
+        gameOverPanel.SetActive(true);
+
+    }
+    void NextLevel()
+    {
+        nextLevel = true;
+        nextLevelPanel.SetActive(true);
+
+    }
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void PlayAgain2()
+    {
+        SceneManager.LoadScene("Game1");
+    }
+    public void Backtomenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+}
---
+++ Assets/Script/ball.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+    public float speed;
+    public Transform explosion;
+    public GameManager gm;
+
+   
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+        rb.AddForce (Vector2.up * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gm.gameOver)
+        {
+            return;
+        }
+        if (inPlay == false)
+        {
+            transform.position = paddle.position;
+        }
+
+
+        
+        if (Input.GetButtonDown("Jump") & inPlay == false)
+        {
+            inPlay = true;
+            rb.AddForce(Vector2.up * speed);
+
+        }
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag ("bottom"))
+        {
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+            gm.UpdateLives(-1);
+        }
+    }
+     void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("brick"))
+        {
+            Transform newEsplosion = Instantiate(explosion, other.transform.position, other.transform.rotation);
+            Destroy(newEsplosion.gameObject, 2.5f);
+
+
+            gm.UpdateScore(1);
+            gm.UpdateNumberOfBricks();
+
+            Destroy(other.gameObject);
+
+        }
+    }
+
+   
+} 
+
